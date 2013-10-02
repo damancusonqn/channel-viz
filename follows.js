afc80e675761be67189b7e830d1ea5d863e9cc72
damancusonqn <damancuso@gmail.com>
2013-10-02 17:23:49 +0100

    Update follows.js
    *Added Raspberry Pi KEY/FEED_ID

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'Ri4NGVpx9WHj95YnPdqRUAgcjAfFtQkKWJjJIEQzxtnVO6hx', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2069858697], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Raspberry Pi Sensors Test#1', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
